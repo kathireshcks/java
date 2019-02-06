5939bb19d3321f9d63a029a0967f31dca248345b
kathireshcks <36156226+kathireshcks@users.noreply.github.com>
2019-02-06 18:26:08 +0530

    Update simplehashmap.java

--- simplehashmap.java
+++ simplehashmap.java
@@ -6,7 +6,7 @@ class array{
 	String name;
 	String email;
 	int id;
-	array next;
+	link next;
 	
 	
 	public array(String name,String email,int id) {
@@ -28,6 +28,44 @@ class array{
 	}
 }
 
+class link{
+	link next;
+	String name,email;
+	int id;
+	
+	link()
+	{
+		name=null;
+		email=null;
+		id=0;
+		next=null;
+	}
+	
+	link(String name,String email,int id)
+	{
+		this.name=name;
+		this.email=email;
+		this.id=id;
+		next=null;
+	}
+	
+	public String getname()
+	{
+		return name;
+	}
+	
+	public String getemail()
+	{
+		return email;
+	}
+	
+	public int id()
+	{
+		return id;
+	}
+}
+
+
 class findhash{
 	
 	public int findarray(String key)
@@ -43,6 +81,29 @@ class findhash{
         total=total%10;
         return total;
 	}
+	
+	public void printdata(link node,String name,String email)
+	{
+		while(node!=null)
+		{
+			if(name.compareTo(node.name)==0 && email.compareTo(node.email)==0)
+			{
+				System.out.println(node.email+" "+node.id);
+				break;
+			}
+			node=node.next;
+		}
+	}
+	
+	public void hashmap(link node,String name,String email,int id)
+	{
+		while(node!=null)
+		{
+			node=node.next;
+		}
+		node.next=new link(name,email,id);
+		
+	}
 }
 
 public class arrayclass {
@@ -56,26 +117,53 @@ public class arrayclass {
 		Scanner input=new Scanner(System.in);
 		
 		a=new array[10];
-		
-		System.out.println("Enter the name : ");
-		String name=input.next();
-		System.out.println("Enter the mail : ");
-		String email=input.next();
-		
-		/*for(int i=0;i<10;i++)
-		{
-			a[i]=new array("kathir"+i,"kokila"+i,i);
-		}*/
-		
-		int out=f.findarray(name);
-		System.out.println(out);
-		
-		a[out]=new array(name,email,out);
-		System.out.println(a[out].name+" : "+a[out].email+" : "+a[out].id);
-		
-//		for(int i=0;i<10;i++)
-//		{
-//			System.out.println(a[i].id+" "+a[i].email+" "+a[i].name);
-//		}
+		int ex;
+		do {
+			
+			System.out.println("1.insert\n2.search\n3.exit");
+			ex=input.nextInt();
+			
+			switch(ex)
+			{
+			case 1:
+				System.out.println("Enter the name : ");
+				String name=input.next();
+				System.out.println("Enter the mail : ");
+				String email=input.next();
+				System.out.println("Enter the id : ");
+				int id=input.nextInt();
+				int out=f.findarray(name);
+				
+				if(a[out]==null)
+					a[out]=new array(name,email,id);
+				else if(a[out].next==null)
+					a[out].next=new link(name,email,id);
+				else 
+					f.hashmap(a[out].next,name,email,id);
+			break;
+			case 2:
+				System.out.println("Enter the name : ");
+				String name1=input.next();
+				System.out.println("Enter the email : ");
+				String email1=input.next();
+				int out1=f.findarray(name1);
+				if(name1.compareTo(a[out1].name)==0 && email1.compareTo(a[out1].email)==0)
+				{
+					System.out.println(a[out1].email+" "+a[out1].id);
+				}else if(a[out1].next!=null)
+				{
+					f.printdata(a[out1].next,name1,email1);
+				}else {
+					System.out.println("No data found");
+				}
+			break;
+			case 3:
+				System.out.println("Exit");
+			break;
+			default:
+				System.out.println("Please select the correct option");
+			break;	
+			}
+		}while(ex!=3);
 	}
 }
